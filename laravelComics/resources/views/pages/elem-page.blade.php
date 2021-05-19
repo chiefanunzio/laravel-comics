41960adfee926d11be59a4f644316036c757f81b
chiefanunzio <chiefa.nunzio@gmail.com>
2021-05-19 18:14:01 +0200

    correzione gianluca!

--- laravelComics/resources/views/pages/elem-page.blade.php
+++ laravelComics/resources/views/pages/elem-page.blade.php
@@ -8,11 +8,11 @@
     <section class="elem-info">
         
         <div>
-            <h1>{{$elem['title']}}</h1>
+            <h1>{{ strtoupper($elem['title']) }}</h1>
 
             <div >
                 <div class="header-tab">
-                    <p>us price:<span>{{$elem['price']}}</span></p>
+                    <p>US Price:<span>{{$elem['price']}}</span></p>
                     <p>AVAILABLE</p>
                 </div>
                 <div class="header-tab">
@@ -30,6 +30,37 @@
             <img src="{{ asset('storage/assets/laravel-comics/adv.jpg')}}" alt="">
         </div>
            
+
+    </section>
+
+    <section class="tab-section">
+        <div>
+            <ul>
+                <li><h2>Talent</h2></li>
+                <li>
+                    <h5>Art by</h5>
+                    <ul>
+                        @foreach ($elem['artists'] as $i)
+                            <li>{{$i}}</li>
+                        @endforeach
+                    </ul>
+                </li>
+            </ul>
+        </div>
+
+        <div>
+            <ul>
+                <li><h2>Talent</h2></li>
+                <li>
+                    <h5>Art by</h5>
+                    <ul>
+                        @foreach ($elem['artists'] as $i)
+                            <li>{{$i}}</li>
+                        @endforeach
+                    </ul>
+                </li>
+            </ul>
+        </div>
 
     </section>
 
